6e99941814c65ccdf7e12f40b05bd92c56394191
Eduardomesut <eduardomesut@gmail.com>
2025-03-15 17:46:14 +0100

    Restructuración del proyecto y añadimos front React con npm

--- spring-rest-skystay/src/main/java/com/example/demo/controllers/ReservaController.java
+++ spring-rest-skystay/src/main/java/com/example/demo/controllers/ReservaController.java
@@ -9,6 +9,8 @@ import com.example.demo.repositories.ReservaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ReservaController {
@@ -32,6 +34,17 @@ public class ReservaController {
 
         Habitacion habitacion = habitacionRepository.findById(habitacionId)
                 .orElseThrow(() -> new RuntimeException("Habitación no encontrada"));
+
+        //Restricción de habitación ocupada esos días
+        List<Reserva> reservaList = reservaRepository.findAll();
+        for (Reserva res:reservaList) {
+            if (res.getHabitacion().getId() == habitacion.getId()){
+                if (!(reserva.getExitDate().isBefore(res.getEntryDate()) || reserva.getEntryDate().isAfter(res.getExitDate()))) {
+                    return ResponseEntity.badRequest().build();
+
+                }
+            }
+        }
 
         // Asignamos cliente y habitación a la reserva
         reserva.setCliente(cliente);
