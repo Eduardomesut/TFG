f44fe211d1700986d6ae7b8eedc5c0c523a56654
Eduardomesut <eduardomesut@gmail.com>
2025-03-31 19:50:47 +0200

    Comenzando api correo

--- spring-rest-skystay/src/main/java/com/example/demo/utils/MailAPI.java
+++ spring-rest-skystay/src/main/java/com/example/demo/utils/MailAPI.java
@@ -1,6 +1,7 @@
 package com.example.demo.utils;
 
 public class MailAPI {
-
+//Aquí hay que hacer la api con metodos de enviar correo al registrase con verificación y otro metodo de codigo canjeado
+    //Tambien otro de el envio de la reserva con la informacion al correo al reservar en usuario
 
 }
